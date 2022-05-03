178c30cdaea8a9ac4882cefed90c6aa3fed79027
Bruno Lima <brunolima.cad@gmail.com>
2022-05-02 23:04:10 -0600

    created auth provider

---
+++ components/contexts/AuthProvider.tsx
@@ -0,0 +1,54 @@
+import { Auth, getAuth, onAuthStateChanged, User } from 'firebase/auth'
+import { doc, onSnapshot } from 'firebase/firestore'
+import React, { createContext, ReactNode, useEffect, useRef, useState } from 'react'
+import { db } from '../../firebase/app'
+import UserModel from '../../models/UserModel'
+
+interface AuthContext {
+   readonly user: UserModel | null
+   readonly fbUser: User | null
+   readonly auth: Auth | null
+}
+
+export const authContext = createContext<AuthContext>({
+   user: null,
+   fbUser: null,
+   auth: null
+})
+
+const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+   const [user, setUser] = useState<UserModel | null>(null)
+   const [fbUser, setFbUser] = useState<User | null>(null)
+   const auth = getAuth()
+
+   let unsubscribe = useRef(() => {})
+
+   useEffect(() => {
+      const authUnsubscribe = onAuthStateChanged(auth, async currentUser => {
+         if (currentUser) setFbUser(currentUser)
+         else setFbUser(null)
+      })
+      return authUnsubscribe
+   }, [])
+
+   useEffect(() => {
+      if (fbUser) {
+         unsubscribe.current = onSnapshot(doc(db, 'users', fbUser.uid), doc => {
+            setUser(doc.data() as UserModel)
+         })
+      }
+      else {
+         unsubscribe.current()
+         unsubscribe.current = () => {}
+      }
+      return unsubscribe.current
+   }, [fbUser])
+
+  return (
+    <authContext.Provider value={{user, fbUser, auth}}>
+       {children}
+    </authContext.Provider>
+  )
+}
+
+export default AuthProvider
