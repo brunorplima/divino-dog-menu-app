fc33bdc42e4401aff4ae4166ef1992b47c8551a9
Bruno Lima <brunolima.cad@gmail.com>
2022-05-02 23:10:38 -0600

    firebase helper funtion; schemas for login and signup forms

---
+++ schemas/authSchemas.ts
@@ -0,0 +1,16 @@
+import * as Yup from 'yup'
+
+const REQUIRED = 'Campo obrigatório'
+const EMAIL_INVALID = 'Email inválido'
+
+export const loginSchema = Yup.object({
+   emailAddress: Yup.string().email().required(REQUIRED),
+   password: Yup.string().min(8, 'Senha deve ter 8 caracteres ou mais').required(REQUIRED)
+})
+
+export const signupSchema = Yup.object({
+   firstName: Yup.string().required(REQUIRED),
+   lastName: Yup.string().required(REQUIRED),
+   emailAddress: Yup.string().email(EMAIL_INVALID).required(REQUIRED),
+   password: Yup.string().min(8, 'Senha deve ter 8 caracteres ou mais').required(REQUIRED),
+})
