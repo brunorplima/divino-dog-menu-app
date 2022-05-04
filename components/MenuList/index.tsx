0ff5fa800bc94df4cded8818793bd3b82a529145
ericbraz <89194596+ericbraz@users.noreply.github.com>
2022-05-04 20:28:43 -0300

    bottom menu layout created

--- components/MenuList/index.tsx
+++ components/MenuList/index.tsx
@@ -21,12 +21,8 @@ export default function MenuList() {
    }
 
    return (
-      <div
-         className={`${style.menuGeneral} px-4 font-medium text-gray-300 bg-gray-700`}
-      >
-         <div
-            className={`${style.fixedMenu} text-white font-extrabold whitespace-nowrap overflow-x-scroll`}
-         >
+      <div className={`${style.menuGeneral} px-4 font-medium text-gray-300`}>
+         <div className={`${style.fixedMenu} text-white font-extrabold whitespace-nowrap overflow-x-scroll`}>
             <div className={`${style.listCategories} z-20`}>
                {excludeEmptyCategory().map((category) => (
                   <Fragment key={category.id}>
@@ -40,14 +36,10 @@ export default function MenuList() {
          </div>
          <hr />
          <br />
-         <div
-            className={`${style.menuContent} max-h-screen overflow-auto inset-y-16 z-10`}
-         >
+         <div className={`${style.menuContent} overflow-auto z-10`}>
             {excludeEmptyCategory().map((category) => (
                <div id={category.name} key={category.id}>
-                  <h2
-                     className={`${style.categories} text-2xl font-extrabold mb-5 text-white`}
-                  >
+                  <h2 className={`${style.categories} text-2xl font-extrabold mb-5 text-white`}>
                      {capitalizeFirstLetter(category.name)}
                   </h2>
                   {menu
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next"
 import MenuList from "../components/MenuList"
+import NavBottom from "../components/NavBottom"
 
 const Home: NextPage = () => {
    return (
-      <div className="m-auto lg:w-1/2" style={ {overflow: "hidden"} }>
+      <div className="m-auto lg:w-1/2">
          <MenuList />
+         <NavBottom />
       </div>
    )
 }
