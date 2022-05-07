c753d16092e105578f9a9edae2846117b2ef8205
Eric Lima <89194596+ericbraz@users.noreply.github.com>
2022-05-07 00:42:11 -0300

    10-create-layout-component-used-as-frame-for-pages (#32)
    * bottom menu layout created
    
    * corrections for layout component
    
    * separating LoaderComponent as its own component
    
    * placing Loader into verse folder

---
+++ components/NavBottom/index.tsx
@@ -0,0 +1,53 @@
+import { Fragment, MouseEvent, useRef, useState } from "react"
+import { BiCart, BiSearchAlt } from "react-icons/bi"
+import { MdRestaurantMenu } from "react-icons/md"
+import { BsPersonCircle } from "react-icons/bs"
+import style from "./NavBottom.module.scss"
+
+export default function NavBottom() {
+   const buttons = [
+      { name: "Cardápio", icon: <MdRestaurantMenu /> },
+      { name: "Pesquisar", icon: <BiSearchAlt /> },
+      { name: "Pedidos", icon: <BiCart /> },
+      { name: "Perfil", icon: <BsPersonCircle /> },
+   ]
+
+   const [theStyle, setTheStyle] = useState("none")
+
+   const buttonTags = useRef<any[]>([])
+   buttonTags.current = []
+   const appendTester = (el: any) => {
+      if (el && !buttonTags.current.includes(el)) buttonTags.current.push(el)
+   }
+
+   const activeClass = (event: MouseEvent) => {
+      const currElId = event.currentTarget.id
+      buttonTags.current.forEach((el) => {
+         el.id === currElId
+            ? (el.className = `${style.button} ${style.active}`)
+            : (el.className = style.button)
+      })
+
+      setTheStyle(`translateX(calc(4.4rem * ${event.currentTarget.id}))`)
+   }
+
+   return (
+      <div
+         className={`${style.navigation} relative bottom-0 flex justify-center items-center h-32 rounded-t-3xl bg-white w-full`}
+      >
+         <div className={`flex`}>
+            {buttons.map((button, index) => (
+               <Fragment key={button.name}>
+                  <div id={String(index)} className={`${style.button} relative ${index === 0 && style.active}`} ref={appendTester} onClick={activeClass}>
+                     <a href="#" className="relative flex justify-center items-center flex-col w-full text-center font-light">
+                        <span className={`${style.icon} relative block text-center`}>{button.icon}</span>
+                        <span className={`${style.text} absolute font-bold tracking-wider opacity-0`}>{button.name}</span>
+                     </a>
+                  </div>
+               </Fragment>
+            ))}
+            <div className={`${style.indicator} absolute`} style={{ transform: theStyle }}></div>
+         </div>
+      </div>
+   )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
-import MenuProvider from '../components/contexts/MenuProvider'
-import AuthProvider from '../components/contexts/AuthProvider'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import "tailwindcss/tailwind.css"
+import MenuProvider from "../components/contexts/MenuProvider"
+import AuthProvider from "../components/contexts/AuthProvider"
+import Layout from "../components/layouts/Layout"
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
       <AuthProvider>
          <MenuProvider>
-            <Component {...pageProps} />
+            <Layout>
+               <Component {...pageProps} />
+            </Layout>
          </MenuProvider>
       </AuthProvider>
    )
