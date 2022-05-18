3c95070777ad9556381741e185fcb26a3479cb84
ericbraz <89194596+ericbraz@users.noreply.github.com>
2022-05-18 14:22:40 -0300

    changes in ListingItems

--- components/MenuList/ListingItems/index.tsx
+++ components/MenuList/ListingItems/index.tsx
@@ -1,20 +1,3 @@
-import style from './Listing.module.scss'
-import MenuItemModel from '../../../models/MenuItemModel'
+import Listing from "./ListingItems";
 
-interface Props {
-   item: MenuItemModel
-}
-
-export default function Listing({ item }: Props) {
-   return (
-      <a href={`/item?itemId=${item.id}&catId=${item.categoryId}`}>
-         <div className={`${style.menuItems} py-5 mb-5 relative`}>
-            <div>
-               <h3 className='font-extrabold'>{item.name}</h3>
-            </div>
-            <div>{item.description}</div>
-            <div className={`${style.neonPrice} absolute bottom-0`}>R$ {item.price}</div>
-         </div>
-      </a>
-   )
-}
+export default Listing
