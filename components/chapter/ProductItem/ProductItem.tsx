2b55df95613e6f07038fbab08368d45ba6e3eb2d
Bruno Lima <brunolima.cad@gmail.com>
2022-05-20 22:14:07 -0600

    added logic in ProductItem to allow users to open modal

--- components/chapter/ProductItem/ProductItem.tsx
+++ components/chapter/ProductItem/ProductItem.tsx
@@ -2,58 +2,69 @@ import React from 'react'
 import { BsCheck } from 'react-icons/bs'
 import { CgUnavailable } from 'react-icons/cg'
 import { priceToString } from '../../../utils/dataHelper'
-import ListItemDetailsEdit from '../../verse/ListItemDetailsEdit/ListItemDetailsEdit'
+import ListItemEditDelete from '../../verse/ListItemEditDelete'
 
 interface Props {
    readonly name: string
    readonly isAvailable: boolean
+   readonly onView?: Function
    readonly price?: number
    readonly img?: string
    readonly description?: string
 }
 
- const ProductItem: React.FC<Props> = ({ img, name, description, price, isAvailable }) => {
+const ProductItem: React.FC<Props> = ({
+   img,
+   name,
+   description,
+   price,
+   isAvailable,
+   onView = () => {},
+}) => {
    return (
       <div className='flex rounded bg-gray-600 py-1 px-3 gap-3'>
-         {!!img && (
-            <div>
-               <img alt={name} src={img} />
-            </div>
-         )}
-         <div className='flex flex-col'>
+         <div className='flex flex-col flex-1 cursor-pointer' onClick={() => onView()}>
             <div className='text-base font-bold text-green-600'>{name}</div>
-            <div>{description}</div>
-            {!!price && <div className='text-green-600'>{priceToString(price)}</div>}
-            <div className='flex'>
-               {isAvailable ? (
-                  <>
-                     <span
-                        className='translate-y-1333 mt-'
-                        style={{
-                           transform: 'translateY(2px)',
-                        }}
-                     >
-                        <BsCheck size={16} color='rgb(22,163,74)' />
-                     </span>{' '}
-                     Em estoque
-                  </>
-               ) : (
-                  <>
-                     <span
-                        className='translate-y-1333 mt-'
-                        style={{
-                           transform: 'translateY(2px)',
-                        }}
-                     >
-                        <CgUnavailable color='#f75' />
-                     </span>{' '}
-                     Indisponível
-                  </>
+            <div className='flex gap-4'>
+               {!!img && (
+                  <div className='w-36 flex items-center'>
+                     <img alt={name} src={img} />
+                  </div>
                )}
+               <div>
+                  <div>{description}</div>
+                  {!!price && <div className='text-green-600'>{priceToString(price)}</div>}
+                  <div className='flex'>
+                     {isAvailable ? (
+                        <>
+                           <span
+                              className='translate-y-1333 mt-'
+                              style={{
+                                 transform: 'translateY(2px)',
+                              }}
+                           >
+                              <BsCheck size={16} color='rgb(22,163,74)' />
+                           </span>{' '}
+                           Em estoque
+                        </>
+                     ) : (
+                        <>
+                           <span
+                              className='translate-y-1333 mt-'
+                              style={{
+                                 transform: 'translateY(2px)',
+                              }}
+                           >
+                              <CgUnavailable color='#f75' />
+                           </span>{' '}
+                           Indisponível
+                        </>
+                     )}
+                  </div>
+               </div>
             </div>
          </div>
-         <div className='flex-1'></div>
-         <ListItemDetailsEdit />
+         <ListItemEditDelete />
       </div>
    )
 }
