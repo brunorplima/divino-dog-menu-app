ded5e99ddfacff404d5fdcf778fa58b60e1bd985
brunorplima <brunolima.cad@gmail.com>
2022-06-18 06:26:04 -0700

    .

--- components/verse/LoaderComponent/LoaderComponent.tsx
+++ components/verse/LoaderComponent/LoaderComponent.tsx
@@ -9,8 +9,8 @@ const LoaderComponent: React.FC<Props> = ({ show, size = 60 }) => (
    <>
       {show && (
          <div
-            className='text-2xl absolute w-screen top-0 left-0 font-bold text-white flex justify-center items-center h-screen z-10'
-            style={{ background: '#222327' }}
+            className='text-2xl absolute w-screen top-0 left-0 font-bold text-white flex justify-center items-center z-10'
+            style={{ background: '#222327', height: 'calc(100vh - 128px)' }}
          >
             <PuffLoader color={'#29fd53'} loading={true} size={size} />
          </div>
