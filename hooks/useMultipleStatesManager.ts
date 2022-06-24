a4bdf02fb4d2ba47eb13a0b0dabf2a0b21bb5764
Eric Lima <89194596+ericbraz@users.noreply.github.com>
2022-06-24 17:12:55 -0300

    creation of select option in in item page (#107)
    * creation of select option in in item page
    
    * type fixing in defineButtonState variable

--- hooks/useMultipleStatesManager.ts
+++ hooks/useMultipleStatesManager.ts
@@ -1,45 +1,49 @@
-import { useState } from "react"
-import { stringToArray } from "../utils/dataHelper"
+import { useState } from 'react'
+import { stringToArray } from '../utils/dataHelper'
 
 interface MultipleStates {
-    id: string
-    subTotal: number
-    state: boolean
-    setState: React.Dispatch<React.SetStateAction<boolean>>
- }
- 
+   id: string
+   subTotal: number
+   state: boolean
+   setState: React.Dispatch<React.SetStateAction<boolean>>
+}
 
 const useMultipleStatesManager = <T extends { id: string; price: number | undefined }>(
-    modelList: T[],
-    url: string | string[] | undefined
- ) => {
-    const lightBoxes: MultipleStates[] = []
-    const appendedLightBoxes = (id: string, price: number, value: boolean) => {
-       const [lightBox, setLightBox] = useState(value)
-       const theStates: MultipleStates = {
-          id: id,
-          subTotal: price,
-          state: lightBox,
-          setState: setLightBox,
-       }
-       lightBoxes.push(theStates)
-    }
-    modelList.forEach((add) => {
-       appendedLightBoxes(
-          add.id,
-          add.price ? add.price : 0,
-          stringToArray(url).includes(add.id)
-       )
-    })
-    const runLightBoxesState = (index: number) => {
-       const currentState = lightBoxes[index]
-       !currentState.state ? currentState.setState(true) : currentState.setState(false)
-    }
+   modelList: T[],
+   url: string | string[] | undefined
+) => {
+   const lightBoxes: MultipleStates[] = []
 
-    return {
-       lightBoxes,
-       runLightBoxesState,
-    }
- }
+   const appendedLightBoxes = (id: string, price: number, value: boolean) => {
+      const [lightBox, setLightBox] = useState(value)
+      const theStates: MultipleStates = {
+         id: id,
+         subTotal: price,
+         state: lightBox,
+         setState: setLightBox,
+      }
+      lightBoxes.push(theStates)
+   }
 
- export default useMultipleStatesManager
+   modelList.forEach((add) => {
+      appendedLightBoxes(add.id, add.price ? add.price : 0, stringToArray(url).includes(add.id))
+   })
+
+   const runLightBoxesState = (index: number, singleOption = false) => {
+      const currentState = lightBoxes[index]
+      !currentState.state ? currentState.setState(true) : currentState.setState(false)
+      if (singleOption) {
+         const tempBoxes = [...lightBoxes]
+         tempBoxes.forEach((box, idx) => {
+            idx !== index && box.setState(false)
+         })
+      }
+   }
+
+   return {
+      lightBoxes,
+      runLightBoxesState,
+   }
+}
+
+export default useMultipleStatesManager
