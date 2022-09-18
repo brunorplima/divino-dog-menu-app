c5e213fc29fc5846951b9e3688e594f4d3100ba5
Bruno Lima <brunolima.cad@gmail.com>
2022-09-17 19:03:02 -0600

    removed unnecessary div tags

--- components/forms/AdminUserEditForm/AdminUserEditForm.tsx
+++ components/forms/AdminUserEditForm/AdminUserEditForm.tsx
@@ -25,81 +25,79 @@ const AdminUserEditForm: React.FC<Props> = ({ user, onSubmit }) => {
    }, [user])
 
    return (
-      <div className=''>
-         <div className=''>
-            <div className='text-lg text-white mb-4'>
-               <div>{user.fullName}</div>
-               <div>{user.emailAddress}</div>
-            </div>
-            <Formik
-               initialValues={initialValue}
-               onSubmit={onSubmit}
-            >
-               {({ values }) => (
-                  <Form>
-                     <SelectFormField
-                        label='Tipo de Usuário'
-                        name='userType'
-                        className='mb-4'
-                        options={[
-                           {
-                              label: REGULAR_USER_TYPE,
-                              value: REGULAR_USER_TYPE
-                           },
-                           {
-                              label: ADMIN_USER_TYPE,
-                              value: ADMIN_USER_TYPE
-                           }
-                        ]}
-                     />
-                     <PrimaryButton
-                        label='Salvar'
-                        disabled={values.userType === initialValue.userType}
-                        clickHandler={() => setDialogOpen(true)}
-                     />
-
-                     <Dialog
-                        id='confirmUserRoleChange'
-                        isOpen={dialogOpen}
-                        onClose={() => setDialogOpen(false)}
-                        footer={[
-                           {
-                              label: 'Cancel',
-                              onClick: () => {
-                                 setDialogOpen(false)
-                              }
-                           },
-                           {
-                              label: 'Ok',
-                              onClick: () => {
-                                 onSubmit(values)
-                                 setDialogOpen(false)
-                              }
-                           }
-                        ]}
-                     >
-                        <>
-                           {values.userType === 'Administrador'
-                              ? (
-                                 <>
-                                    <p>Tem certeza que quer transformar {user.fullName} em um Administrador?</p>
-                                    <p>{user.fullName} terá acesso a esse painél de administradores e poderá modificar seu banco de dados.</p>
-                                 </>
-                              )
-                              : (
-                                 <>
-                                    <p>Tem certeza que quer remover a condição de administrador de {user.fullName}?</p>
-                                    <p>{user.fullName} perderá acesso a esse painél de administradores e não poderá mais modificar seu banco de dados.</p>
-                                 </>
-                              )
-                           }
-                        </>
-                     </Dialog>
-                  </Form>
-               )}
-            </Formik>
+      <>
+         <div className='text-lg text-white mb-4'>
+            <div>{user.fullName}</div>
+            <div>{user.emailAddress}</div>
          </div>
-      </div>
+         <Formik
+            initialValues={initialValue}
+            onSubmit={onSubmit}
+         >
+            {({ values }) => (
+               <Form>
+                  <SelectFormField
+                     label='Tipo de Usuário'
+                     name='userType'
+                     className='mb-4'
+                     options={[
+                        {
+                           label: REGULAR_USER_TYPE,
+                           value: REGULAR_USER_TYPE
+                        },
+                        {
+                           label: ADMIN_USER_TYPE,
+                           value: ADMIN_USER_TYPE
+                        }
+                     ]}
+                  />
+                  <PrimaryButton
+                     label='Salvar'
+                     disabled={values.userType === initialValue.userType}
+                     clickHandler={() => setDialogOpen(true)}
+                  />
+
+                  <Dialog
+                     id='confirmUserRoleChange'
+                     isOpen={dialogOpen}
+                     onClose={() => setDialogOpen(false)}
+                     footer={[
+                        {
+                           label: 'Cancel',
+                           onClick: () => {
+                              setDialogOpen(false)
+                           }
+                        },
+                        {
+                           label: 'Ok',
+                           onClick: () => {
+                              onSubmit(values)
+                              setDialogOpen(false)
+                           }
+                        }
+                     ]}
+                  >
+                     <>
+                        {values.userType === 'Administrador'
+                           ? (
+                              <>
+                                 <p>Tem certeza que quer transformar {user.fullName} em um Administrador?</p>
+                                 <p>{user.fullName} terá acesso a esse painél de administradores e poderá modificar seu banco de dados.</p>
+                              </>
+                           )
+                           : (
+                              <>
+                                 <p>Tem certeza que quer remover a condição de administrador de {user.fullName}?</p>
+                                 <p>{user.fullName} perderá acesso a esse painél de administradores e não poderá mais modificar seu banco de dados.</p>
+                              </>
+                           )
+                        }
+                     </>
+                  </Dialog>
+               </Form>
+            )}
+         </Formik>
+      </>
    )
 }
 
